9501153ab05af3f35d7f4164dfa36a5b0ef71247
ArgenisValdez <argenisvalde80@gmail.com>
2024-11-18 01:16:00 -0400

    Creacion de formularios

--- GestorApp/Program.cs
+++ GestorApp/Program.cs
@@ -17,7 +17,7 @@ namespace GestorApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormConectar());
+            Application.Run(new FormEmpleados());
         }
     }
 }
